a45dbba4c52d5f1f7666999461d6fb9491c62ada
Nouma <nofonkoue@gmail.com>
2026-01-31 12:57:05 -0600

    Fixed Constants, started README

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,9 +34,9 @@ public final class Constants {
 
         // Chassis configuration
 
-        public static final double kTrackWidth = Units.inchesToMeters(25.25);
+        public static final double kTrackWidth = Units.inchesToMeters(23.5);
         // Distance between centers of right and left wheels on robot
-        public static final double kWheelBase = Units.inchesToMeters(25.25);
+        public static final double kWheelBase = Units.inchesToMeters(23.5);
 
         // Distance between front and back wheels on robot
         public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
@@ -74,7 +74,7 @@ public final class Constants {
         // The MAXSwerve module can be configured with one of three pinion gears: 12T,
         // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
         // more teeth will result in a robot that drives faster).
-        public static final int kDrivingMotorPinionTeeth = 14;
+        public static final int kDrivingMotorPinionTeeth = 13;
 
         // Calculations required for driving motor conversion factors and feed forward
         public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
