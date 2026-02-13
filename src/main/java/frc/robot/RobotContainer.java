77f7b3d3840b9187f17add38236d7c5d6791bd96
Nouma <nofonkoue@gmail.com>
2026-02-12 21:22:25 -0600

    Separate Targeting Command - Nouma

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.DriveCommand;
+import frc.robot.commands.TargetCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
@@ -73,22 +74,28 @@ public class RobotContainer {
      * {@link JoystickButton}.
      */
     private void configureButtonBindings() {
-
         // The right stick controls translation of the robot.
         // Turning is controlled by the X axis of the left stick.
+
         m_robotDrive.setDefaultCommand(new DriveCommand(
-                rightJoystick::getY,
-                rightJoystick::getX,
-                leftJoystick::getX,
-                leftJoystick::getY,
-                () -> rightJoystick.trigger().getAsBoolean(),
-                m_robotDrive));
+                    rightJoystick::getY,
+                    rightJoystick::getX,
+                    leftJoystick::getX,
+                    leftJoystick::getY,
+                    m_robotDrive));
+
+        rightJoystick.trigger().whileTrue(new TargetCommand(rightJoystick::getY,
+                    rightJoystick::getX,
+                    leftJoystick::getX,
+                    leftJoystick::getY, 
+                    m_robotDrive));
 
         // Button 7 on the right stick resets the gyro
         rightJoystick.button(7).onTrue(
                 new InstantCommand(m_robotDrive::zeroHeading));
-                
+
     }
+
 
     /**
      * Use this method to set up all autonomous routines and add them to a
---
+++ src/main/java/frc/robot/commands/TargetCommand.java
@@ -0,0 +1,105 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.Meter;
+
+import java.util.Optional;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+/** Default command for driving the {@code DriveSubystem} using joysticks */
+public class TargetCommand extends Command {
+
+    private final DriveSubsystem driveSubsystem;
+    private final DoubleSupplier forwardStick;
+    private final DoubleSupplier sidewaysStick;
+    private final DoubleSupplier rotStick; 
+    private final DoubleSupplier rotSticky; 
+    private final Translation2d R_hub = new Translation2d(4.02844, 3.522);
+    private final Translation2d B_hub = new Translation2d(16.54 - 4.02844, 8.07 - 3.522);
+    private final PIDController targetLockPID = new PIDController(1, 0, 0);
+    
+    /**
+     * Drive the robot using joysticks.
+     * 
+     * @param forwardStick Joystick for forward translation.
+     * @param sidewaysStick Joystick for sideways translation.
+     * @param rotStick Joystick axis for rotation.
+     * @param rotSticky
+     * @param drive DriveSubsystem
+     */
+    public TargetCommand(DoubleSupplier forwardStick, DoubleSupplier sidewaysStick, DoubleSupplier rotStick, DoubleSupplier rotSticky,
+            DriveSubsystem drive) {
+        this.forwardStick = forwardStick;
+        this.sidewaysStick = sidewaysStick;
+        this.rotStick = rotStick;
+        this.rotSticky = rotSticky;
+        this.driveSubsystem = drive;
+        addRequirements(this.driveSubsystem);
+    }
+
+    private Translation2d getTargetHub() {
+        Optional<Alliance> al = DriverStation.getAlliance();
+        if(al.get() == DriverStation.Alliance.Blue){
+            return R_hub;
+        }
+        else{
+            return B_hub;
+        }
+    }
+
+    @Override
+    public void execute() {
+        double xSpeed = MathUtil.applyDeadband(forwardStick.getAsDouble(), OIConstants.kDriveDeadband) * -1;
+        double ySpeed = MathUtil.applyDeadband(sidewaysStick.getAsDouble(), OIConstants.kDriveDeadband);
+  
+        double rot = MathUtil.applyDeadband(rotStick.getAsDouble(), OIConstants.kDriveDeadband) * -1;
+        if (RobotBase.isSimulation()){
+            ySpeed = MathUtil.applyDeadband(sidewaysStick.getAsDouble(), OIConstants.kDriveDeadband) * -1;
+            xSpeed = MathUtil.applyDeadband(forwardStick.getAsDouble(), OIConstants.kDriveDeadband);
+            rot = MathUtil.applyDeadband(rotStick.getAsDouble(), OIConstants.kDriveDeadband);
+        }    
+        Pose2d targetpose = new Pose2d(getTargetHub(), new Rotation2d());
+        if (targetpose != null) {
+            Pose2d currentpose = driveSubsystem.getPose();
+            Rotation2d ang = currentpose.getRotation();
+            Distance y = currentpose.getMeasureY();
+            Distance x = currentpose.getMeasureX();
+            Distance tagx = targetpose.getMeasureX();
+            Distance tagy = targetpose.getMeasureY();
+            Distance deltax = tagx.minus(x);
+            Distance deltay = tagy.minus(y);
+            Double ang_to_target = Math.atan2(deltay.in(Meter), deltax.in(Meter));
+            Rotation2d angle_to_target_radians = new Rotation2d(ang_to_target);
+            Rotation2d relative_rotation = ang.relativeTo(angle_to_target_radians);
+            double raw_rot = -targetLockPID.calculate(relative_rotation.getRadians(), 0);
+            System.out.print(raw_rot);
+            if (raw_rot < -1){
+                rot = -1;
+            }
+            if (raw_rot > 1){
+                rot = 1;
+            }
+            else{
+                rot = raw_rot;
+            }
+        }
+        rot = rot * -1;
+                driveSubsystem.drive(xSpeed, ySpeed, rot, true);
+    }
+}
