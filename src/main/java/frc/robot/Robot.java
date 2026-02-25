f481b8bf1de35731aad5b2beb0966e67fd167489
EdwardHarder <Edward.harder2@gmail.com>
2026-02-25 04:45:33 -0600

    oops frogot to stage

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -77,7 +77,6 @@ public class Robot extends TimedRobot {
     @Override
     public void autonomousInit() {
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-        m_robotContainer.configureButtonBindings();
         // schedule the autonomous command (example)
         if (m_autonomousCommand != null) {
             CommandScheduler.getInstance().schedule(m_autonomousCommand);
@@ -100,6 +99,7 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
+        m_robotContainer.configureButtonBindings();
         Elastic.selectTab("Teleoperated");
     }
 
