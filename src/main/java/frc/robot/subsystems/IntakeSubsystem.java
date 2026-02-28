e2fa3c8eb9fa65011b762d98b6f3d27426522dbc
ahumanhuman <wyatt.schwick@gmail.com>
2026-02-27 18:47:05 -0600

    deploy stuff not done but there

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,20 +5,28 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs.IntakeConfig;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-    SparkMax m_intakeMotor = new SparkMax(IntakeConstants.kIntakeCanId, MotorType.kBrushless);
-    SparkMax m_deployLeftMotor = new SparkMax(IntakeConstants.kDeployLeftCanId, MotorType.kBrushless);
-    SparkMax m_deployRightMotor = new SparkMax(IntakeConstants.kDeployRightCanId, MotorType.kBrushless);
-
+    private final SparkMax m_intakeMotor = new SparkMax(IntakeConstants.kIntakeCanId, MotorType.kBrushless);
+    private final SparkMax m_deployLeftMotor = new SparkMax(IntakeConstants.kDeployLeftCanId, MotorType.kBrushless);
+    private final SparkMax m_deployRightMotor = new SparkMax(IntakeConstants.kDeployRightCanId, MotorType.kBrushless);
+    private final PIDController m_control = new PIDController(1, 0, 0); // find good pid values
+    private final double out_position = 0.0; //read encoders to get proper value
+    private final double in_position = 0.0;  //same
+    private RelativeEncoder m_leftEncoder = m_deployLeftMotor.getEncoder();
+    private RelativeEncoder m_rightEncoder = m_deployRightMotor.getEncoder();
+    private double set_point;
     /** Creates a new IntakeSubsystem. */
     public IntakeSubsystem() {
         m_intakeMotor.configure(IntakeConfig.kIntakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
@@ -26,11 +34,21 @@ public class IntakeSubsystem extends SubsystemBase {
         m_deployLeftMotor.configure(IntakeConfig.kDeployLeftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         m_deployRightMotor.configure(IntakeConfig.kDeployRightConfig.follow(m_deployLeftMotor),
             ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+        m_leftEncoder = m_deployLeftMotor.getEncoder();
+        m_rightEncoder = m_deployRightMotor.getEncoder();
+        // m_leftEncoder.setPosition(0);
+        // m_rightEncoder.setPosition(0);
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        double lmotor_speed = MathUtil.clamp(-0.1 + m_control.calculate(m_leftEncoder.getPosition(), set_point), -1.0,1.0);
+        double rmotor_speed = MathUtil.clamp(-0.1 + m_control.calculate(m_rightEncoder.getPosition(), set_point), -1.0,1.0);
+        m_deployLeftMotor.set(lmotor_speed);
+        m_deployRightMotor.set(rmotor_speed);
+
     }
 
     public void setIntakeSpeed(double speed) {
@@ -40,11 +58,13 @@ public class IntakeSubsystem extends SubsystemBase {
 
     /** Deploys the intake out */
     public void deployOut() {
-        // m_deployLeftMotor.
+        set_point = out_position;
+        m_control.reset();
     }
 
     /** Retracts the intake back */
     public void deployIn() {
-
+        set_point = in_position;
+        m_control.reset();
     }
 }
